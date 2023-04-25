fb62a579a65aa4613677cf6dac914ef1b676a314
CHARMI PATEL <61372206+charmipatel1305@users.noreply.github.com>
2023-04-25 14:17:04 -0500

    Update bc-erm_radnet.py

--- bc-erm_radnet.py
+++ bc-erm_radnet.py
@@ -534,7 +534,7 @@ def test(epoch):
         
         
 # number of epochs
-n_epochs = 3
+n_epochs = 68
 tr_loss = []
 tr_accuracies = []
 val_loss = []
@@ -545,7 +545,7 @@ max_worst_accuracy = 0
 
 patience = 1 
 best_val_loss = float('inf')
-num_trials = 1
+num_trials = 30
 df1 = pd.DataFrame(columns=['trial', 'subtype', 'Train ERM accuracy'])
 df2 = pd.DataFrame(columns=['trial', 'subtype', 'Val ERM accuracy'])
 df3 = pd.DataFrame(columns=['trial', 'subtype', 'Test ERM accuracy'])
